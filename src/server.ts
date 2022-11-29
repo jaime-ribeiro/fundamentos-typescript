32a677a70ba3eb8408a141e39abb0beff4a73d0f
jaime-ribeiro <littlejou@gmail.com>
2022-11-29 19:54:02 -0300

    Create folder and change route :construction:

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,11 @@
 import express, { response } from "express";
-
-import { createCourse } from "./routes";
+import { categoriesRoutes } from "./routes/categories.routes";
 
 const app = express();
 
 //Funcionamento de json
 app.use(express.json());
 
-//app.get("/", createCourse);
-
-app.get("/", (request,response) =>{
-    return response.json({message: "Hello World"});
-});
-
-app.post("/courses", (request, response) => {
-    const {name} = request.body;
-    
-    return response.json({name})
-});
+app.use(categoriesRoutes);
 
 app.listen(3333);
