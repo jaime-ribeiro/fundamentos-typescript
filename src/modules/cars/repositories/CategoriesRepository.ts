d362ffd6d5b90caa5c52c12089074b6904344e72
jaime-ribeiro <littlejou@gmail.com>
2022-12-12 20:57:28 -0300

    listCategories Fixed :bug:

--- src/modules/cars/repositories/CategoriesRepository.ts
+++ src/modules/cars/repositories/CategoriesRepository.ts
@@ -7,9 +7,20 @@ import {
 class CategoriesRepository implements ICategoriesRepository {
   private categories: Category[];
 
-  constructor() {
+  // eslint-disable-next-line no-use-before-define
+  private static INSTANCE: CategoriesRepository;
+
+  private constructor() {
     this.categories = [];
   }
+
+  public static getInstance(): CategoriesRepository {
+    if (!CategoriesRepository.INSTANCE) {
+      CategoriesRepository.INSTANCE = new CategoriesRepository();
+    }
+    return CategoriesRepository.INSTANCE;
+  }
+  // Método criado que é responsável por instanciar o repositório, caso não tenha instanciado antes ele vai criar, caso ele já tenha ele só vai prosseguir
 
   // Quando quisermos passar o tipo de retorno da função criada, teremos que usar ":" como é usado depois do create(...): void
   create({ name, description }: ICreateCategoryDTO): void {
--- src/modules/cars/useCases/createCategory/index.ts
+++ src/modules/cars/useCases/createCategory/index.ts
@@ -2,8 +2,8 @@ import { CategoriesRepository } from "../../repositories/CategoriesRepository";
 import { CreateCategoryController } from "./CreateCategoryController";
 import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
 
-const categoriesRepository = new CategoriesRepository();
-
+const categoriesRepository = CategoriesRepository.getInstance();
+// A instancia ficou global com o pattern utilizado e não será mais necessária, além de ter a criação de ter seu construtor privado.
 const createCategoryUseCase = new CreateCategoryUseCase(categoriesRepository);
 
 const createCategoryController = new CreateCategoryController(
